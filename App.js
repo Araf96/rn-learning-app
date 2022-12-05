f2511a5c4b6d3fbf69e231c7a1dd8e738bca9471
Araf96 <rafatspn@gmail.com>
2022-12-05 16:40:43 +0600

    scrolling view added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -25,11 +25,11 @@ export default function App() {
         />
         <Button title="Add" onPress={courseGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal, id) => (
           <View key={id} style={styles.listItem}><Text>{goal}</Text></View>
         ))}
-      </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
