6e0a738ac6ca217fa84b693e466dc75ea0d224b9
Araf96 <rafatspn@gmail.com>
2022-12-05 15:58:35 +0600

    manage state added for course goals

--- App.js
+++ App.js
@@ -3,11 +3,22 @@ import { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const enteredGoalHandler = (val) =>{
+    setEnteredGoal(val)
+  }
+
+  const courseGoalHandler = () => {
+    setCourseGoals(currGoals => [...courseGoals, enteredGoal])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="Add" />
+        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={enteredGoalHandler} value={enteredGoal} />
+        <Button title="Add" onPress={courseGoalHandler}/>
       </View>
       <View></View>
       <StatusBar style="auto" />
